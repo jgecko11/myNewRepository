0e1d7dcd6930457c12704d4fb1d9c1e4303bedbb
jgecko11 <jgreco22@kentdenver.org>
2020-09-29 16:28:13 -0600

    Update NodeClass.java
    More instance data, two constructor classes, and set methods for info, and both pointers

--- NodeClass.java
+++ NodeClass.java
@@ -1,6 +1,31 @@
 public class NodeClass {
-    private Object node; //Private Instance Data
+    private Object node; //Private Instance Data for node
+    private Object pointer1; //First Pointer
+    private Object pointer2; //Second Pointer
 
     public  NodeClass(){} //Empty constructor
+
+    public NodeClass(Object initialData){ //constructor for just data
+        node = initialData;
+    }
+
+    public NodeClass(Object data, Object pointer1, Object pointer2){ //constructor for info and pointers
+        this.node = data;
+        this.pointer1 = pointer1;
+        this.pointer2 = pointer2;
+    }
+
+    public void setNode(Object updatedData){ //set data
+        this.node = updatedData;
+    }
+
+    public void setPointer1(Object updtaedPointer1){ //set first pointer
+        pointer1 = updtaedPointer1;
+    }
+
+    public void setPointer2(Object updatedPointer2){ //set second pointer
+        pointer2 = updatedPointer2;
+    }
+
 
 }
